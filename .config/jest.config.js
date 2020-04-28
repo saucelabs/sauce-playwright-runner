9f333db0a5c1800e555017fa955b1f78b899b676
christian-bromann <mail@christian-bromann.com>
2020-04-28 16:10:13 +0200

    minor adjustments

--- .config/jest.config.js
+++ .config/jest.config.js
@@ -5,6 +5,7 @@ const ROOT = path.resolve(__dirname, '..')
 module.exports = {
     rootDir: ROOT,
     testEnvironment: 'node',
+    testRunner: 'jest-circus/runner',
     setupFilesAfterEnv: [
         `${ROOT}/src/jest.setup.js`,
         `${ROOT}/src/jest.teardown.js`
--- src/jest.setup.js
+++ src/jest.setup.js
@@ -2,13 +2,7 @@ const path = require('path')
 
 const got = require('got')
 const { chromium } = require('playwright')
-const debug = require(
-    path.join(
-        path.dirname(require.resolve('playwright')),
-        'node_modules',
-        'debug'
-    )
-)
+const debug = require('debug')
 
 const { CHROME_DEFAULT_PATH, JEST_TIMEOUT, CHROME_ARGS } = require('./constants')
 const { logHelper } = require('./utils')
@@ -19,15 +13,21 @@ jest.setTimeout(process.env.JEST_TIMEOUT || JEST_TIMEOUT)
 beforeAll(async () => {
     global.browser = await chromium.launch({
         headless: !Boolean(process.env.DISPLAY),
-        args: CHROME_ARGS,
-        executablePath: process.env.CHROME_BINARY_PATH || CHROME_DEFAULT_PATH
+        args: CHROME_ARGS
     }).catch((err) => {
         console.error(`Couldn't start Playwright: ${err.message}`)
     })
 
+    // Create a new incognito browser context.
+    global.context = await browser.newContext();
+    // Create a new page in a pristine context.
+    global.page = await context.newPage();
+
     const req = got('http://localhost:9223/json')
     const pages = await req.json()
-    console.log(`Watch test: https://chrome-devtools-frontend.appspot.com/serve_file/@ec99b9f060de3f065f466ccd2b2bfbf17376b61e/devtools_app.html?ws=localhost:9222/devtools/page/${pages[0].id}&remoteFrontend=true`);
+    if (pages.length) {
+        console.log(`Watch test: https://chrome-devtools-frontend.appspot.com/serve_file/@ec99b9f060de3f065f466ccd2b2bfbf17376b61e/devtools_app.html?ws=localhost:9222/devtools/page/${pages[0].id}&remoteFrontend=true`);
+    }
 })
 
 const monkeyPatchedTest = (origFn) => (testName, testFn) => {
