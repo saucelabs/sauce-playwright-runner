7c726c46f01a422f6bb58f98237874acce598f3b
Felix P <felix.pescarmona@saucelabs.com>
2020-10-23 17:20:17 -0700

    Record output with script

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,9 @@ exports.HOME_DIR = '/home/seluser'
 exports.CHROME_DEFAULT_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 exports.DEFAULT_JEST_TIMEOUT = 60 // 1min
 
+exports.PLAYWRIGHT_SCRIPT = exports.HOME_DIR + '/playwright.typescript'
+exports.PLAYWRIGHT_LOG = exports.HOME_DIR + '/playwright.log'
+
 const LOG_DIR = '/var/log/cont'
 exports.LOG_FILES = [
     LOG_DIR + '/chrome_browser.log',
@@ -15,7 +18,8 @@ exports.LOG_FILES = [
     LOG_DIR + '/xvfb-tryouts-stderr.log',
     LOG_DIR + '/xvfb-tryouts-stdout.log',
     exports.HOME_DIR + '/videos/video.mp4',
-    exports.HOME_DIR + '/docker.log'
+    exports.HOME_DIR + '/docker.log',
+    exports.PLAYWRIGHT_LOG,
 ]
 
 exports.SUPPORTED_BROWSER = ['chromium', 'firefox', 'webkit']
