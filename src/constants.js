c4485a9e250b9a5693709cd2752032e4d69f8867
Felix P <felix.pescarmona@saucelabs.com>
2020-10-26 15:15:18 -0700

    Use __dirname to put file

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,6 @@ exports.HOME_DIR = '/home/seluser'
 exports.CHROME_DEFAULT_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 exports.DEFAULT_JEST_TIMEOUT = 60 // 1min
 
-exports.PLAYWRIGHT_LOG = exports.HOME_DIR + '/playwright.log'
-
 const LOG_DIR = '/var/log/cont'
 exports.LOG_FILES = [
     LOG_DIR + '/chrome_browser.log',
@@ -18,7 +16,7 @@ exports.LOG_FILES = [
     LOG_DIR + '/xvfb-tryouts-stdout.log',
     exports.HOME_DIR + '/videos/video.mp4',
     exports.HOME_DIR + '/docker.log',
-    exports.PLAYWRIGHT_LOG,
+    exports.HOME_DIR + '/playwright.log',
 ]
 
 exports.SUPPORTED_BROWSER = ['chromium', 'firefox', 'webkit']
