e271d809caefd9fc5b5ffef32dcfa65282fe5220
Dan <dpgraham.85@gmail.com>
2020-12-04 12:48:44 -0800

    fixes

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,19 +1,16 @@
 set -e
 rm -rf ./bundle/
 mkdir ./bundle/
-export CYPRESS_CACHE_FOLDER=$PWD/bundle/Cache/
-echo $CYPRESS_CACHE_FOLDER
+export PLAYWRIGHT_BROWSERS_PATH=$PWD/bundle/Cache/
+echo $PLAYWRIGHT_BROWSERS_PATH
 cp -r ./src/ ./bundle/src/
 cp -r bin/ bundle/bin/
 cp package.json bundle/package.json
 cp package-lock.json bundle/package-lock.json
-cp config.yaml bundle/config.yaml
-cp cypress.json bundle/cypress.json
 cp "$(which node)" bundle/
 
 pushd bundle/
 npm cache clean --force
 npm ci --production
-./node ./node_modules/cypress/bin/cypress verify
 # TODO: Add "saucectl" tests here
 popd
