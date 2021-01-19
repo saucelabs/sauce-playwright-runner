46c86ca05ff23f88ef88a375672810a457dfeefa
dpgraham <dpgraham.85@gmail.com>
2021-01-19 14:16:13 -0800

    fix

---
+++ tests/run.sh
@@ -0,0 +1,8 @@
+# TODO: Uncomment these when saucectl supports the new format
+#echo "RUNNING BASIC-JS"
+#$SAUCE_CTL_BINARY run --config  ./tests/fixtures/basic-js/.sauce/config.yml
+#echo "STATUS CODE: $?"
+
+#echo "RUNNING BASIC-TS"
+#$SAUCE_CTL_BINARY run --config  ./tests/fixtures/basic-js/.sauce/config.yml
+#echo "STATUS CODE: $?"
