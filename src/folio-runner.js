7b288b4c4f74da89161706f38fb5d8be2cf7bc91
Félix P <felix@saucelabs.com>
2021-02-24 18:50:13 -0800

    Move out previously nested funcs

--- src/folio-runner.js
+++ src/folio-runner.js
@@ -14,6 +14,101 @@ const { shouldRecordVideo, getAbsolutePath, toHyphenated, getArgs, exec } = util
 const region = process.env.SAUCE_REGION || 'us-west-1';
 const jobName = process.env.SAUCE_JOB_NAME || `DevX Playwright Test Run - ${(new Date()).getTime()}`;
 
+
+async function createJob (hasPassed, startTime, endTime, args, playwright) {
+  const api = new SauceLabs({
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    region
+  });
+  const cwd = process.cwd();
+
+  let tags = process.env.SAUCE_TAGS;
+  if (tags) {
+    tags = tags.split(',');
+  }
+  let sessionId;
+  if (process.env.ENABLE_DATA_STORE) {
+    // TODO: When we enable this make sure it's getting the proper parameters
+    sessionId = await createJobShell(tags, api);
+  } else {
+    sessionId = await createJobWorkaround(tags, api, hasPassed, startTime, endTime, args, playwright);
+  }
+  const containerLogFiles = LOG_FILES.filter(
+    (path) => fs.existsSync(path));
+
+  // Take the 1st webm video we find and translate it video.mp4
+  // TODO: We need to translate all .webm to .mp4 and combine them into one video.mp4
+  const webmFiles = glob.sync(path.join(cwd, '__assets__', '**', '*.webm'));
+  let videoLocation;
+  if (webmFiles.length > 0) {
+    videoLocation = path.join(cwd, '__assets__', 'video.mp4');
+    try {
+      await exec(`ffmpeg -i ${webmFiles[0]} ${videoLocation}`, {suppressLogs: true});
+    } catch (e) {
+      videoLocation = null;
+      console.error(`Failed to convert ${webmFiles[0]} to mp4: '${e}'`);
+    }
+  }
+
+  let files = [
+    path.join(cwd, 'console.log'),
+    path.join(cwd, '__assets__', 'junit.xml'), // TOOD: Should add junit.xml.json as well
+    ...containerLogFiles
+  ];
+
+  if (videoLocation) {
+    files.push(videoLocation);
+  }
+
+  await Promise.all([
+    api.uploadJobAssets(sessionId, {files})
+      .then(
+        (resp) => {
+          if (resp.errors) {
+            for (let err of resp.errors) {
+              console.error(err);
+            }
+          }
+        },
+        (e) => console.error('upload failed:', e.stack)
+      ),
+    api.updateJob(process.env.SAUCE_USERNAME, sessionId, {
+      name: jobName,
+      passed: hasPassed
+    })
+  ]);
+
+  return sessionId;
+}
+
+
+async function runReporter ({ hasPassed, startTime, endTime, args, playwright }) {
+  let jobDetailsUrl, reportingSucceeded = false;
+  try {
+    let sessionId = await createJob(hasPassed, startTime, endTime, args, playwright);
+    let domain;
+    switch (region) {
+      case 'us-west-1':
+        domain = 'saucelabs.com';
+        break;
+      case 'staging':
+        domain = 'ondemand.staging.saucelabs.net';
+        break;
+      default:
+        domain = `${region}.saucelabs.com`;
+    }
+
+    reportingSucceeded = true;
+    jobDetailsUrl = `https://app.${domain}/tests/${sessionId}`;
+    console.log(`\nOpen job details page: ${jobDetailsUrl}\n`);
+  } catch (e) {
+    console.log(`Failed to upload results to Sauce Labs. Reason: ${e.message}`);
+  } finally {
+    utils.updateExportedValueToSaucectl({ jobDetailsUrl, reportingSucceeded });
+  }
+}
+
 async function run (nodeBin, runCfgPath, suiteName) {
   runCfgPath = getAbsolutePath(runCfgPath);
   const runCfg = await utils.loadRunConfig(runCfgPath);
@@ -88,98 +183,6 @@ async function run (nodeBin, runCfgPath, suiteName) {
     });
   });
 
-  const api = new SauceLabs({
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
-    region
-  });
-
-  async function createJob (hasPassed, startTime, endTime, args, playwright) {
-    let tags = process.env.SAUCE_TAGS;
-    if (tags) {
-      tags = tags.split(',');
-    }
-    let sessionId;
-    if (process.env.ENABLE_DATA_STORE) {
-      // TODO: When we enable this make sure it's getting the proper parameters
-      sessionId = await createJobShell(tags, api);
-    } else {
-      sessionId = await createJobWorkaround(tags, api, hasPassed, startTime, endTime, args, playwright);
-    }
-    const containerLogFiles = LOG_FILES.filter(
-      (path) => fs.existsSync(path));
-
-    // Take the 1st webm video we find and translate it video.mp4
-    // TODO: We need to translate all .webm to .mp4 and combine them into one video.mp4
-    const webmFiles = glob.sync(path.join(cwd, '__assets__', '**', '*.webm'));
-    let videoLocation;
-    if (webmFiles.length > 0) {
-      videoLocation = path.join(cwd, '__assets__', 'video.mp4');
-      try {
-        await exec(`ffmpeg -i ${webmFiles[0]} ${videoLocation}`, {suppressLogs: true});
-      } catch (e) {
-        videoLocation = null;
-        console.error(`Failed to convert ${webmFiles[0]} to mp4: '${e}'`);
-      }
-    }
-
-    let files = [
-      path.join(cwd, 'console.log'),
-      path.join(cwd, '__assets__', 'junit.xml'), // TOOD: Should add junit.xml.json as well
-      ...containerLogFiles
-    ];
-
-    if (videoLocation) {
-      files.push(videoLocation);
-    }
-
-    await Promise.all([
-      api.uploadJobAssets(sessionId, {files})
-        .then(
-          (resp) => {
-            if (resp.errors) {
-              for (let err of resp.errors) {
-                console.error(err);
-              }
-            }
-          },
-          (e) => console.error('upload failed:', e.stack)
-        ),
-      api.updateJob(process.env.SAUCE_USERNAME, sessionId, {
-        name: jobName,
-        passed: hasPassed
-      })
-    ]);
-
-    return sessionId;
-  }
-
-  async function runReporter ({ hasPassed, startTime, endTime, args, playwright }) {
-    let jobDetailsUrl, reportingSucceeded = false;
-    try {
-      let sessionId = await createJob(hasPassed, startTime, endTime, args, playwright);
-      let domain;
-      switch (region) {
-        case 'us-west-1':
-          domain = 'saucelabs.com';
-          break;
-        case 'staging':
-          domain = 'ondemand.staging.saucelabs.net';
-          break;
-        default:
-          domain = `${region}.saucelabs.com`;
-      }
-
-      reportingSucceeded = true;
-      jobDetailsUrl = `https://app.${domain}/tests/${sessionId}`;
-      console.log(`\nOpen job details page: ${jobDetailsUrl}\n`);
-    } catch (e) {
-      console.log(`Failed to upload results to Sauce Labs. Reason: ${e.message}`);
-    } finally {
-      utils.updateExportedValueToSaucectl({ jobDetailsUrl, reportingSucceeded });
-    }
-  }
-
   let startTime, endTime, hasPassed = false;
   try {
     startTime = new Date().toISOString();
@@ -198,6 +201,7 @@ async function run (nodeBin, runCfgPath, suiteName) {
     console.log('Skipping asset uploads! Remember to setup your SAUCE_USERNAME/SAUCE_ACCESS_KEY');
     return hasPassed;
   }
+
   runReporter({ hasPassed, startTime, endTime, args, playwright: runCfg.playwright });
   return hasPassed;
 }
