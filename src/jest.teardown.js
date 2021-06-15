56b8b4ee6a36cfb11dc373230ea42d2297992363
Alex Plischke <alex.plischke@saucelabs.com>
2021-06-15 10:23:00 -0700

    Remove obsolete jest files

--- src/jest.teardown.js
+++
@@ -1,6 +0,0 @@
-afterAll(async function () {
-  if (global.browser) {
-    await global.browser.close().catch(
-            (err) => console.error(`Failed to close browser: ${err.message}`));
-  }
-});
