47f24fb8134ff22dae3a53b18a8e43a3496a1158
Félix P <felix@saucelabs.com>
2021-06-24 13:47:05 +0200

    Change it to test

--- tests/fixtures/basic-js/tests/test.spec.js
+++ tests/fixtures/basic-js/tests/test.spec.js
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const { it, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-it('is a basic test with the page', async ({ page }) => {
+test('is a basic test with the page', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   expect(await page.innerText('.navbar__title')).toBe('Playwright');
 });
