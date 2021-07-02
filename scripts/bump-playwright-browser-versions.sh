16618bedd4f0dfd60dd47ee7ffdb25c337e10ca4
Mike Han <mike.han@saucelabs.com>
2021-07-02 13:18:03 -0600

    Manually commit the bumped browsers.json
    Instead of using release-it for consistency with other runner release
    workflows.

--- scripts/bump-playwright-browser-versions.sh
+++ scripts/bump-playwright-browser-versions.sh
@@ -5,3 +5,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 NODE_PATH=$SCRIPT_DIR/../node_modules node $SCRIPT_DIR/print-versions.js > $SCRIPT_DIR/../browsers.json
+
+# Don't use release-it to commit update to browsers.json. This is for consistency with other runner
+# release processes.
+git add . --update
+git commit --message "Bump playwright browser versions"
