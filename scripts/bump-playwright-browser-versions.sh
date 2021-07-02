77be185010e809f45fe59d16ef40139de7d06959
Mike Han <mike.han@saucelabs.com>
2021-07-02 10:57:43 -0600

    Remove the explicit `git add` in the bump browser version script
    release-it handles adding files to the index so no need to do it
    manually.

--- scripts/bump-playwright-browser-versions.sh
+++ scripts/bump-playwright-browser-versions.sh
@@ -5,6 +5,3 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 NODE_PATH=$SCRIPT_DIR/../node_modules node $SCRIPT_DIR/print-versions.js > $SCRIPT_DIR/../browsers.json
-
-# Since this script runs as an `after:bump` hook with release-it, add to git index so release-it can commit it
-git add $SCRIPT_DIR/../browsers.json
