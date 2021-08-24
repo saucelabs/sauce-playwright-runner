c56658181d3b9f9c69540c5bf1e41085fb87878b
Mike Han <mike.han@saucelabs.com>
2021-08-24 13:46:56 -0600

    Consolidate config
    Drive all playwright config with a single config file. It sets defaults
    and merges with customer defined playwright.config.js file (if it
    exists).

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,28 @@
-module.exports = {
+const process = require('process');
+const _ = require('lodash');
+
+let custom = {};
+try {
+  // Import the user's config if it exists
+  custom = require('./playwright.config.js');
+} catch {
+}
+
+const defaults = {
   use: {
-    video: 'on',
+    headed: process.env.SAUCE_VM ? true : false,
+    video: process.env.SAUCE_VM ? 'off' : 'on',
   },
+  reporter: [
+    ['line'],
+    ['junit', { outputFile: 'junit.xml' }],
+  ],
 };
+
+if ('HTTP_PROXY' in process.env && process.env.HTTP_PROXY !== '') {
+  defaults.use.proxy = {
+    server: process.env.HTTP_PROXY,
+  };
+}
+
+module.exports = _.defaultsDeep(defaults, custom);
