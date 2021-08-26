79065a55dbc1c98b18cff050a584f7b36e4c683c
Mike Han <mike.han@saucelabs.com>
2021-08-26 15:07:21 -0600

    use regex to account for platform diffs?

--- src/playwright-recorder.js
+++ src/playwright-recorder.js
@@ -4,13 +4,15 @@ const path = require('path');
 const { Transform } = require('stream');
 const childProcess = require('child_process');
 
+const escapeSequenceRegex = new RegExp('[\\u001b]\\[2K|[\\u001b]\\[0G', 'g');
+
 function playwrightRecorder () {
   // console.log is saved out of reportsDir since it is cleared on startup.
   const ws = fs.createWriteStream(path.join(process.cwd(), 'console.log'), { flags: 'w+', mode: 0o644 });
   const stripAsciiTransform = new Transform({
     transform (chunk, encoding, callback) {
       // list reporter uses escape codes to rewrite lines, strip them to make console output more readable
-      callback(null, chunk.toString().replace('\u001b[2K\u001b[0G', ''));
+      callback(null, chunk.toString().replace(escapeSequenceRegex, ''));
     },
   });
 
