c7ac73f60c3d046bc8c7032de0244d3da69aca52
Mike Han <mike.han@saucelabs.com>
2021-09-10 15:35:05 -0600

    cull dead code

--- src/utils.js
+++ src/utils.js
@@ -4,87 +4,12 @@ const path = require('path');
 const yargs = require('yargs/yargs');
 
 const log = logger('utils');
-const sendString = 'SEND ► ';
-const receiveString = '◀ RECV';
-
-global._timeOfLastCommand = Date.now();
-
-// eslint-disable-next-line no-unused-vars
-function logHelper (type, severity, message, args, hints) {
-  if (message.includes(receiveString)) {
-    const line = message.slice(message.indexOf('{'));
-
-    try {
-      const response = JSON.parse(line);
-      if (!response.id) {
-        return;
-      }
-
-      const log = global.logs.find(
-                (log) => parseInt(log.id, 10) === parseInt(response.id, 10));
-
-      if (log) {
-        log.result = response;
-      }
-    } catch (e) {
-      log.error(`Couldn't parse Playwright logs: ${e.stack}`);
-    }
-
-    return;
-  }
-
-  if (!message.includes(sendString)) {
-    return;
-  }
-
-  const line = message.slice(message.indexOf(sendString) + sendString.length);
-
-  try {
-    const command = JSON.parse(line);
-    const duration = (Date.now() - global._timeOfLastCommand) / 1000;
-    global.logs.push({
-      id: command.id,
-      screenshot: 0,
-      between_commands: duration,
-      start_time: Date.now() / 1000,
-      suggestion_values: [],
-      request: command.params,
-      HTTPStatus: 200,
-      result: '',
-      suggestion: null,
-      duration,
-      path: command.method,
-      in_video_timeline: 0,
-      method: 'SOCKET',
-      statusCode: 0
-    });
-  } catch (e) {
-    /**
-         * some lines can't be parsed due to borked Playwright log lines, e.g.:
-         * "{"id":6 [evaluate injected script]}"
-         */
-    log.error(`Couldn't parse log line: ${line}`);
-  }
-  global._timeOfLastCommand = Date.now();
-}
 
 function getAbsolutePath (pathToDir) {
   if (path.isAbsolute(pathToDir)) {
     return pathToDir;
   }
   return path.join(process.cwd(), pathToDir);
-}
-
-function shouldRecordVideo () {
-  if (process.env.SAUCE_VM) {
-    return false;
-  }
-  let isVideoRecording = process.env.SAUCE_VIDEO_RECORDING;
-  if (isVideoRecording === undefined) {
-    return true;
-  }
-  let videoOption = String(isVideoRecording).toLowerCase();
-  return videoOption === 'true' || videoOption === '1';
 }
 
 /**
@@ -106,9 +31,8 @@ function toHyphenated (str) {
   return out.join('');
 }
 
-const COMMAND_TIMEOUT = 5000;
-
 function exec (expression, {suppressLogs = false}) {
+  const COMMAND_TIMEOUT = 5000;
   const cp = shell.exec(expression, { async: true, silent: true });
   if (!suppressLogs) {
     cp.stdout.on('data', (data) => log.info(`${data}`));
@@ -144,10 +68,6 @@ function getArgs () {
   return { nodeBin, runCfgPath, suiteName };
 }
 
-async function supportsFfmpeg () {
-
-}
-
 function replaceLegacyKeys (args) {
   // browserName => browser
   if ('browserName' in args) {
@@ -167,5 +87,5 @@ function replaceLegacyKeys (args) {
 }
 
 module.exports = {
-  exec, logHelper, shouldRecordVideo, getAbsolutePath, toHyphenated,
-  getArgs, supportsFfmpeg, replaceLegacyKeys };
+  exec, getAbsolutePath, toHyphenated,
+  getArgs, replaceLegacyKeys };
