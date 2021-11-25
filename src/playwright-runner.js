b168104fb06101e98533554022153782c8850323
Mike Han <56001373+mhan83@users.noreply.github.com>
2021-11-25 14:09:02 -0700

    [DEVX-1337] Add sauce report to uploads (#129)
    * Configure the sauce reporter
    
    * add playwright reporter
    
    * Configure sauce playwright reporter to not upload
    
    Just use it for report generation for now.
    
    * Actually upload the generated sauce report

--- src/playwright-runner.js
+++ src/playwright-runner.js
@@ -59,6 +59,7 @@ async function createJob (suiteName, hasPassed, startTime, endTime, args, playwr
   let files = [
     path.join(cwd, 'console.log'),
     path.join(assetsDir, 'junit.xml'),
+    path.join(assetsDir, 'sauce-test-report.json'),
     ...containerLogFiles
   ];
 
@@ -222,6 +223,7 @@ async function runReporter ({ suiteName, hasPassed, startTime, endTime, args, pl
 async function run (nodeBin, runCfgPath, suiteName) {
   const assetsDir = path.join(process.cwd(), '__assets__');
   const junitFile = path.join(assetsDir, 'junit.xml');
+  const sauceReportFile = path.join(assetsDir, 'sauce-test-report.json');
 
   runCfgPath = getAbsolutePath(runCfgPath);
   const runCfg = await loadRunConfig(runCfgPath);
@@ -283,6 +285,7 @@ async function run (nodeBin, runCfgPath, suiteName) {
     ...process.env,
     ...suite.env,
     PLAYWRIGHT_JUNIT_OUTPUT_NAME: junitFile,
+    SAUCE_REPORT_OUTPUT_NAME: sauceReportFile,
     FORCE_COLOR: 0,
   };
 
--- src/sauce.config.js
+++ src/sauce.config.js
@@ -22,6 +22,13 @@ const overrides = {
     // outputFile is set by playwright-runner.js as an env variable. The runner needs to process it
     // so better for it to set the output path
     ['junit'],
+    // outputFile is set by playwright-runner.js as an env variable. The runner needs to process it
+    // so better for it to set the output path
+    ['@saucelabs/playwright-reporter',
+      {
+        upload: false,
+      },
+    ],
   ],
 };
 
