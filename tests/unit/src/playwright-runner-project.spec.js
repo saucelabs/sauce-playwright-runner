de5877eb89c97fdc281b5e1a7b530c83835696b8
Tian Feng <tian.feng@saucelabs.com>
2021-11-30 12:58:42 -0800

    update unit test

--- tests/unit/src/playwright-runner-project.spec.js
+++ tests/unit/src/playwright-runner-project.spec.js
@@ -5,7 +5,7 @@ jest.mock('glob');
 jest.mock('sauce-testrunner-utils');
 jest.mock('../../../src/reporter');
 const path = require('path');
-const { run, generateJunitfile } = require('../../../src/playwright-runner');
+const { run } = require('../../../src/playwright-runner');
 const childProcess = require('child_process');
 const { EventEmitter } = require('events');
 const SauceLabs = require('saucelabs').default;
@@ -15,7 +15,7 @@ const testRunnerUtils = require('sauce-testrunner-utils');
 
 const MOCK_CWD = '/fake/runner';
 
-describe('playwright-runner', function () {
+describe('playwright-runner for playwright project', function () {
   const baseRunCfg = {
     playwright: {
       version: '1.12.2',
@@ -27,7 +27,8 @@ describe('playwright-runner', function () {
         param: {
           browserName: 'chromium',
           headful: true,
-          slowMo: 1000
+          slowMo: 1000,
+          project: 'project1'
         },
         testMatch: ['**/*.spec.js', '**/*.test.js']
       }
@@ -35,10 +36,11 @@ describe('playwright-runner', function () {
   };
 
   describe('.run', function () {
-    let spawnMock, playwrightProc, backupEnv, fsExistsMock, cwdMock;
+    let spawnMock, spawnMock2, playwrightProc, backupEnv, fsExistsMock, cwdMock;
     beforeEach(function () {
       backupEnv = {};
       spawnMock = jest.spyOn(childProcess, 'spawn');
+      spawnMock2 = jest.spyOn(childProcess, 'spawn');
       fsExistsMock = jest.spyOn(fs, 'existsSync');
       cwdMock = jest.spyOn(process, 'cwd');
       playwrightProc = new EventEmitter();
@@ -56,6 +58,12 @@ describe('playwright-runner', function () {
         }, 10);
         return playwrightProc;
       });
+      spawnMock2.mockImplementation(() => {
+        setTimeout(() => {
+          playwrightProc.emit('close', 0);
+        }, 10);
+        return playwrightProc;
+      });
       fsExistsMock.mockImplementation((url) => url.startsWith('/bad/path') ? false : true);
       cwdMock.mockReturnValue(MOCK_CWD);
       process.env = {
@@ -68,7 +76,7 @@ describe('playwright-runner', function () {
       process.env = backupEnv;
     });
 
-    it('should run playwright test as a spawn command in VM', async function () {
+    it('should run playwright project test', async function () {
       process.env.SAUCE_VM = 'truthy';
       testRunnerUtils.loadRunConfig.mockReturnValue({...baseRunCfg});
       await run('/fake/path/to/node', path.join(MOCK_CWD, 'sauce-runner.json'), 'basic-js');
@@ -84,10 +92,10 @@ describe('playwright-runner', function () {
         path.join(MOCK_CWD, '__assets__'),
         '--config',
         path.join(MOCK_CWD, 'sauce.config.js'),
+        '--project',
+        'project1',
         '--timeout',
         1800000,
-        '--browser',
-        'chromium',
         '--headed',
         '**/*.spec.js',
         '**/*.test.js',
@@ -102,16 +110,5 @@ describe('playwright-runner', function () {
         'stdio': 'inherit',
       });
     });
-  });
-
-  describe('.generateJunit', function () {
-    const junitPath = 'tests/unit/src/__assets__/junit.xml';
-    const backupContent = fs.readFileSync(junitPath, 'utf8');
-
-    generateJunitfile('tests/unit/src/', 'Firefox using global mode', 'firefox');
-
-    expect(fs.readFileSync(junitPath)).toEqual(fs.readFileSync('tests/unit/src/__assets__/expected_junit.xml'));
-
-    fs.writeFileSync(junitPath, backupContent);
   });
 });
