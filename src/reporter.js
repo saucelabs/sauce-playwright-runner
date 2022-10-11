415118183ef49bced565397548bf30702c0a2893
Alex Plischke <alex.plischke@saucelabs.com>
2022-10-11 15:08:24 -0700

    Use new library for Test-Composer interactions

--- src/reporter.js
+++ src/reporter.js
@@ -1,9 +1,9 @@
-const { DESIRED_BROWSER } = require('./constants');
+const {DESIRED_BROWSER} = require('./constants');
 
-async function createJobReport (runCfg, api, passed, startTime, endTime, saucectlVersion) {
+async function createJobReport(runCfg, testComposer, passed, startTime, endTime) {
   /**
-     * don't try to create a job if no credentials are set
-     */
+   * don't try to create a job if no credentials are set
+   */
   if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
     return;
   }
@@ -13,33 +13,26 @@ async function createJobReport (runCfg, api, passed, startTime, endTime, saucect
     browserVersion = runCfg.playwright.version;
   }
 
-  const body = {
-    name: runCfg.suite.name,
-    user: process.env.SAUCE_USERNAME,
-    startTime,
-    endTime,
-    framework: 'playwright',
-    frameworkVersion: process.env.PLAYWRIGHT_VERSION,
-    status: 'complete',
-    suite: runCfg.suite.name,
-    errors: [],
-    passed,
-    tags: runCfg.sauce.metadata?.tags,
-    build: runCfg.sauce.metadata?.build,
-    browserName,
-    browserVersion,
-    platformName: process.env.IMAGE_NAME + ':' + process.env.IMAGE_TAG,
-    saucectlVersion,
-  };
-
-  let resp;
+  let job;
   try {
-    resp = await api.createJob(body);
+    job = await testComposer.createReport({
+      name: runCfg.suite.name,
+      startTime,
+      endTime,
+      framework: 'playwright',
+      frameworkVersion: process.env.PLAYWRIGHT_VERSION,
+      passed,
+      tags: this.opts.tags,
+      build: this.opts.build,
+      browserName,
+      browserVersion,
+      platformName: process.env.IMAGE_NAME + ':' + process.env.IMAGE_TAG
+    });
   } catch (e) {
-    console.error('Create job failed: ', e.stack);
+    console.error('Failed to create job:', e);
   }
 
-  return resp?.ID || 0;
+  return job;
 }
 
 module.exports.createJobReport = createJobReport;
