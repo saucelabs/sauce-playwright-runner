d8cb0ba8abeb342cc6a3b5504a484df5f5e009b1
Alex Plischke <alex.plischke@saucelabs.com>
2022-10-12 09:03:20 -0700

    Use new library for Test-Composer interactions (#175)
    * Clean up dead code
    
    * Update docs
    
    * Add test-composer lib
    
    * Use new library for Test-Composer interactions
    
    * Bump playwright-reporter
    
    * Use the same node version across releases
    
    * Optimize pipeline
    
    * Remove improper unit tests
    
    * Remove obsolete dependency
    
    * Bump dependencies
    
    * Remove redundant call
    
    * Use correct header
    
    * Pass the correct tag and builds information
    
    * Install cucumber ondemand

--- src/reporter.js
+++ src/reporter.js
@@ -1,66 +1,9 @@
-const { DESIRED_BROWSER } = require('./constants');
+const {DESIRED_BROWSER} = require('./constants');
 
-// NOTE: this function is not available currently.
-// It will be ready once data store API actually works.
-// Keep these pieces of code for future integration.
-const createJobReportV2 = async (suiteName, metadata, api) => {
-  const body = {
-    name: suiteName,
-    acl: [
-      {
-        type: 'username',
-        value: process.env.SAUCE_USERNAME
-      }
-    ],
-    //'start_time: startTime,
-    //'end_time: endTime,
-    source: 'vdc', // will use devx
-    platform: 'webdriver', // will use playwright
-    status: 'complete',
-    live: false,
-    metadata: {},
-    tags: metadata.tags, // TODO add 'build' information once API stabilizes
-    attributes: {
-      container: false,
-      browser: DESIRED_BROWSER,
-      browser_version: DESIRED_BROWSER.toLowerCase() === 'firefox' ? process.env.FF_VER : process.env.CHROME_VER,
-      commands_not_successful: 1, // to be removed
-      devx: true,
-      os: 'test', // need collect
-      performance_enabled: 'true', // to be removed
-      public: 'team',
-      record_logs: true, // to be removed
-      record_mp4: 'true', // to be removed
-      record_screenshots: 'true', // to be removed
-      record_video: 'true', // to be removed
-      video_url: 'test', // remove
-      log_url: 'test' // remove
-    }
-  };
-
-  let sessionId;
-  await Promise.all([
-    api.createResultJob(
-      body
-    ).then(
-      (resp) => {
-        sessionId = resp.id;
-      },
-      (e) => {
-        console.error('Create job failed: ', e.stack);
-      }
-    )
-  ]);
-
-  return sessionId || 0;
-};
-
-// TODO Tian: this method is a temporary solution for creating jobs via test-composer.
-// Once the global data store is ready, this method will be deprecated.
-async function createJobReport (runCfg, api, passed, startTime, endTime, saucectlVersion) {
+async function createJobReport(runCfg, testComposer, passed, startTime, endTime) {
   /**
-     * don't try to create a job if no credentials are set
-     */
+   * don't try to create a job if no credentials are set
+   */
   if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
     return;
   }
@@ -70,34 +13,26 @@ async function createJobReport (runCfg, api, passed, startTime, endTime, saucect
     browserVersion = runCfg.playwright.version;
   }
 
-  const body = {
-    name: runCfg.suite.name,
-    user: process.env.SAUCE_USERNAME,
-    startTime,
-    endTime,
-    framework: 'playwright',
-    frameworkVersion: process.env.PLAYWRIGHT_VERSION,
-    status: 'complete',
-    suite: runCfg.suite.name,
-    errors: [],
-    passed,
-    tags: runCfg.sauce.metadata?.tags,
-    build: runCfg.sauce.metadata?.build,
-    browserName,
-    browserVersion,
-    platformName: process.env.IMAGE_NAME + ':' + process.env.IMAGE_TAG,
-    saucectlVersion,
-  };
-
-  let resp;
+  let job;
   try {
-    resp = await api.createJob(body);
+    job = await testComposer.createReport({
+      name: runCfg.suite.name,
+      startTime,
+      endTime,
+      framework: 'playwright',
+      frameworkVersion: process.env.PLAYWRIGHT_VERSION,
+      passed,
+      tags: runCfg.sauce.metadata?.tags,
+      build: runCfg.sauce.metadata?.build,
+      browserName,
+      browserVersion,
+      platformName: process.env.IMAGE_NAME + ':' + process.env.IMAGE_TAG
+    });
   } catch (e) {
-    console.error('Create job failed: ', e.stack);
+    console.error('Failed to create job:', e);
   }
 
-  return resp?.ID || 0;
+  return job;
 }
 
-module.exports.createJobReportV2 = createJobReportV2;
 module.exports.createJobReport = createJobReport;
---
+++ tests/unit/src/playwright-runner.spec.cjs
@@ -0,0 +1,16 @@
+jest.mock('fs');
+const {generateJunitfile} = require('../../../src/playwright-runner');
+const fs = require('fs');
+
+describe('playwright-runner', function () {
+  describe('.generateJunit', function () {
+    const junitPath = 'tests/unit/src/__assets__/junit.xml';
+    const backupContent = fs.readFileSync(junitPath, 'utf8');
+
+    generateJunitfile('tests/unit/src/', 'Firefox using global mode', 'firefox');
+
+    expect(fs.readFileSync(junitPath)).toEqual(fs.readFileSync('tests/unit/src/__assets__/expected_junit.xml'));
+
+    fs.writeFileSync(junitPath, backupContent);
+  });
+});
