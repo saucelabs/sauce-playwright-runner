85ab85ac8b961621f0dbad5d5f916076787d5ce5
Felix P <felix@saucelabs.com>
2023-03-20 09:46:49 +0100

    Merge sauce-connect tests

---
+++ tests/fixtures/pre-release/cucumber/features/support/steps.js
@@ -0,0 +1,38 @@
+const { Before, When, Then } = require('@cucumber/cucumber');
+const { chromium, firefox, webkit } = require('playwright');
+const { expect } = require('@playwright/test');
+const prettySeconds = require('pretty-seconds');
+
+Before(async function () {
+  const opts = {
+    headless: false,
+    recordVideo: {
+      dir: 'assets'
+    }
+  };
+  switch (process.env.BROWSER_NAME) {
+    case 'firefox':
+      this.browser = await firefox.launch(opts);
+      break;
+    case 'webkit':
+      this.browser = await webkit.launch(opts);
+      break;
+    default:
+      this.browser = await chromium.launch(opts);
+      break;
+  }
+  const context = await this.browser.newContext();
+  this.page = await context.newPage();
+});
+
+When('I open {string} with chromium', async function (string) {
+  await this.page.goto(string);
+  await expect(this.page).toHaveTitle(/Google/);
+  expect(prettySeconds(80)).toBe('1 minute and 20 seconds');
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+});
+
+Then('Close chromium', async function () {
+  await this.browser.close();
+});
+
