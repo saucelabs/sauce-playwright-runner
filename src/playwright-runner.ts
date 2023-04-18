fe9c0f39d1f07aad61046ec2fa0964ac035875e7
Mike Han <mike.han@saucelabs.com>
2023-04-18 10:51:16 -0600

    export function under test

--- src/playwright-runner.ts
+++ src/playwright-runner.ts
@@ -142,7 +142,7 @@ function getPlatformName(platformName: string) {
   return platformName;
 }
 
-function generateJunitfile(sourceFile: string, suiteName: string, browserName: string, platformName: string) {
+export function generateJunitfile(sourceFile: string, suiteName: string, browserName: string, platformName: string) {
   if (!fs.existsSync(sourceFile)) {
     return;
   }
