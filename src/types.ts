d06c5141f17ff6288fec8212773ada5c1245d343
Tian Feng <tian.feng@saucelabs.com>
2024-11-25 10:38:46 -0800

    give formatOptions more precise type

--- src/types.ts
+++ src/types.ts
@@ -101,7 +101,7 @@ export interface CucumberSuite {
     import?: string[];
     tags?: string[];
     format?: string[];
-    formatOptions?: object;
+    formatOptions?: { [key: string]: string };
     parallel?: number;
     paths: string[];
   };
