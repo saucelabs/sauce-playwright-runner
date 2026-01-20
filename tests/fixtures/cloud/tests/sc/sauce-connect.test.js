b2e7d5b0d7614dda74445256c03eb192fdc07b53
Nitin <nitin.kumar@saucelabs.com>
2026-01-20 22:14:30 +0530

    revert sauce connect version

--- tests/fixtures/cloud/tests/sc/sauce-connect.test.js
+++ tests/fixtures/cloud/tests/sc/sauce-connect.test.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@playwright/test');
 
 test('sauce-connect is working', async ({ page }) => {
   await page.goto('http://127.0.0.1:8000/');
-  await expect(page).toHaveTitle('Simple Page');
+  expect(await page.innerText('title')).toBe('Simple Page');
 });
