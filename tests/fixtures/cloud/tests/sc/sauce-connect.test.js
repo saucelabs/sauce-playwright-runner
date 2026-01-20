1ef90ff33736c6f61195d38e2c91cccd575ced02
Nitin <nitin.kumar@saucelabs.com>
2026-01-20 22:01:13 +0530

    change test case for sauce connect

--- tests/fixtures/cloud/tests/sc/sauce-connect.test.js
+++ tests/fixtures/cloud/tests/sc/sauce-connect.test.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@playwright/test');
 
 test('sauce-connect is working', async ({ page }) => {
   await page.goto('http://127.0.0.1:8000/');
-  expect(await page.innerText('title')).toBe('Simple Page');
+  await expect(page).toHaveTitle('Simple Page');
 });
