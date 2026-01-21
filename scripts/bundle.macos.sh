e8b900029e8659c4a63a5dc875fef506962ffaf3
nitinkumar-saucelabs <nitin.kumar@saucelabs.com>
2026-01-21 15:56:49 +0530

    Feat/macos webkit and sauce connect v5 (#309)
    * change locator value for exact match
    
    * change macos version to macos-15-intel
    
    * change macos version to macos-15-intel
    
    * bump the playwright version to 1.57
    
    * downgrade the playwright version to 1.56.1 from 1.57
    
    * add macos15 on build test
    
    * change version to 15.7.0 and add playwright install for macos13
    
    * change version to 15.7.0 and add playwright install for macos13
    
    * added updated package-lock
    
    * installing in bundle cache
    
    * remove macos bundle file
    
    * added debug logs for track
    
    * updated sauce connect to v5
    
    * change test case for sauce connect
    
    * revert sauce connect version
    
    * change test case for sauce connect
    
    * revert sauce connect version
    
    * updated sauce connect version to 5 and modify test case
    
    * allow localhost proxy
    
    * revert a list bundle step in release.yml file
    
    * create seperate bundle.sh for macos and windows
    
    * moved archive logic to the respective file

---
+++ scripts/bundle.macos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Run common bundling steps
+bash ./scripts/bundle.sh
+
+export PLAYWRIGHT_BROWSERS_PATH=$PWD/bundle/Cache/
+export PLAYWRIGHT_SKIP_BROWSER_GC=1
+
+pushd bundle/
+
+# Install all browsers with mac13 platform override
+PLAYWRIGHT_HOST_PLATFORM_OVERRIDE=mac13 npx playwright install
+PLAYWRIGHT_HOST_PLATFORM_OVERRIDE=mac13 npx playwright install-deps
+npx playwright --version
+
+popd
+
+# Archive Bundle with symlinks
+zip --symlinks -r playwright-macos-amd64.zip bundle/
---
+++ scripts/bundle.windows.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Run common bundling steps
+bash ./scripts/bundle.sh
+
+export PLAYWRIGHT_BROWSERS_PATH=$PWD/bundle/Cache/
+
+pushd bundle/
+
+# Install browsers for Windows
+npx playwright install
+npx playwright install-deps
+npx playwright --version
+
+popd
+
+# DEVX-560: Add missing vcruntime140_1.dll
+# Related to:
+# - https://github.com/microsoft/playwright/issues/4293
+# - https://github.com/microsoft/playwright/pull/4973
+# The upgrade to playwright 1.8.0 does not fix the missing
+# DLL issue. As a workaround, we decided to ship it within
+# the bundle to avoid modifiying the system image.
+cp -n ./libs/vcruntime140_1.dll ${PLAYWRIGHT_BROWSERS_PATH}/firefox-*/firefox/
+
+# Archive Bundle using PowerShell
+powershell.exe -Command "Compress-Archive -Path bundle/ -DestinationPath playwright-windows-amd64.zip -Force"
