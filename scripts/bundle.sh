19bf1ab03fb43778f20122f2d38447b8781215c7
Nitin <nitin.kumar@saucelabs.com>
2026-01-21 13:25:38 +0530

    create seperate bundle.sh for macos and windows

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -21,19 +21,6 @@ pushd bundle/
 npm cache clean --force
 npm ci --omit=dev
 
-
-npx playwright install
-npx playwright install-deps
-npx playwright --version
 # TODO: Add "saucectl" tests here
 
 popd
-
-# DEVX-560: Add missing vcruntime140_1.dll
-# Related to:
-# - https://github.com/microsoft/playwright/issues/4293
-# - https://github.com/microsoft/playwright/pull/4973
-# The upgrade to playwright 1.8.0 does not fix the missing
-# DLL issue. As a workaround, we decided to ship it within
-# the bundle to avoid modifiying the system image.
-cp -n ./libs/vcruntime140_1.dll ${PLAYWRIGHT_BROWSERS_PATH}/firefox-*/firefox/
